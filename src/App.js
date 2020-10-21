fba0d1591234a7361fc196f0dd6db9db57d86273
Keith Garcia <Keith.Garcia444@gmail.com>
2020-10-21 11:48:29 -0400

    its done

--- src/App.js
+++ src/App.js
@@ -9,19 +9,22 @@ import LandingPage from './components/LandingPage';
 import EncounterView from './components/Encounter';
 import PokeMartView from './components/PokeMart'
 import PokedexView from './components/Pokedex';
+import FourZeroFour from './components/404/404';
+
 
 // const token = localStorage.getItem('token')
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token') || '')
-  const [pokemonTrainer, setPokemonTrainer] = useState({username: '', password: ''});
+  const [pokemonTrainer, setPokemonTrainer] = useState({ username: '', password: '' });
   return (
     <Router>
       <Switch>
         <Route exact path="/pokedex"><PokedexView /></Route>
         <Route exact path="/pokemart"><PokeMartView /></Route>
-        <Route exact path="/encounter" render={() => !token ? <Redirect to="/" /> : <EncounterView  setToken={setToken} />} />
+        <Route exact path="/encounter" render={() => !token ? <Redirect to="/" /> : <EncounterView setToken={setToken} />} />
         <Route exact path="/" render={() => token ? <Redirect to="/encounter" /> : <LandingPage token={token} setToken={setToken} pokemonTrainer={pokemonTrainer} setPokemonTrainer={setPokemonTrainer} />} />
+        <Route render={() => <FourZeroFour />} />
       </Switch>
     </Router>
   )
---
+++ src/components/404/404.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FourZeroFour() {
+    return (
+        <>
+            <h2>404 page not found</h2>
+            <a href="/">Home</a>
+        </>
+    )
+}
